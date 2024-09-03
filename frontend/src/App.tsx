37ea8f9bc17cec65adcac8ffdac0f3ee851d837f
sneharani1818 <sneharani1818@gmail.com>
2024-09-03 21:23:20 +0530

    Created frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>eLearning Platform under development.</h1>
+    <h2>Server is working, UI is being designed and will be available soon.</h2>
+        </>
+  )
+}
+
+export default App
